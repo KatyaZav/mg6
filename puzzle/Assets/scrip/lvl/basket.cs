fcc4d435868fdf72fa1c718b12d277547d387e48
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2023-10-12 12:04:41 +0500

    base 4

--- puzzle/Assets/scrip/lvl/basket.cs
+++ puzzle/Assets/scrip/lvl/basket.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class basket : MonoBehaviour
 {
     public TypePlayer collecteblePlayer;
-    
+
+    public GameObject goodEffect;
+    public AudioClip goodClip;
+    public GameObject badEffect;
+    public AudioClip badClip;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var pla = collision.gameObject.GetComponent<player>();
@@ -15,10 +19,12 @@ public class basket : MonoBehaviour
         if (pla.type == collecteblePlayer)
         {
             Debug.Log("Good");
+            Instantiate(goodEffect, collision.gameObject.transform.position, Quaternion.identity);
         }
         else
         {
             Debug.Log("Loose");
+            Instantiate(badEffect, collision.gameObject.transform.position, Quaternion.identity);
         }
     }
 }
